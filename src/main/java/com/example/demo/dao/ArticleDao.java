f8cd1c646bdacbb3a2cdfa92146888634fe28237
alphathx13 <alphathx13@gmail.com>
2024-06-19 16:28:01 +0900

    2일차 중간

--- src/main/java/com/example/demo/dao/ArticleDao.java
+++ src/main/java/com/example/demo/dao/ArticleDao.java
@@ -1,72 +1,74 @@
 package com.example.demo.dao;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.stereotype.Component;
 
 import com.example.demo.vo.Article;
 
+@Component
 public class ArticleDao {
+	private static int lastArticleId;
+	private static List<Article> articles;
 	
-	public static List<Article> articleWrite(int lastArticleId, List<Article> articles, String title, String body) {
-		articles.add(new Article(lastArticleId++, title, body));
+	public ArticleDao(ArticleDao articleDao) {
+		lastArticleId = 1;
+		articles = new ArrayList<>();
+		
+		makeTestData();
+	}
+	
+	public void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			String title = i + "번 게시글";
+			String body = i + "번 게시글 내용";
+			articles.add(new Article(lastArticleId++, title, body));
+		}
+	}
+	
+	public Article articleWrite(String title, String body) {
+		Article article = new Article(lastArticleId++, title, body);
+		articles.add(article);
 
+		return article;
+	}
+	
+	public List<Article> articleList() {
 		return articles;
 	}
 	
-	public static void articleList() {
-	}
-	
-	public static String articleDetail(int id) {
+	public Object articleDetail(int id) {
 		Article foundArticle = getArticleById(id);
 
 		if (foundArticle == null) 
 			return id + "번 게시글은 존재하지 않습니다.";
 		
 		return foundArticle;
-		
-		if (id != null) {
-			return id.getId() + "  " + id.getTitle() + "  " + id.getBody();
-		}
-		
-		return "해당 게시글은 존재하지 않습니다.";
 	}
 	
-	public static String articleModify(int id, String title, String body) {
+	public String articleModify(int id, String title, String body) {
 		Article foundArticle = getArticleById(id);
 		
 		if (foundArticle == null) 
 			return id + "번 게시글은 존재하지 않습니다.";
 
-		modifyArticle(foundArticle, title, body);
-		
+		foundArticle.setTitle(title);
+		foundArticle.setBody(body);
 		return id + "번 게시글을 수정하였습니다.";
-		if (id != null) {
-			id.setTitle(title);
-			id.setBody(body);
-			return id.getId() + "번 게시글을 수정하였습니다.";
-		}
-
-		return "해당 게시글은 존재하지 않습니다.";
 	}
 	
-	public static String articleDelete(Article article) {
+	public String articleDelete(int id) {
 		Article foundArticle = getArticleById(id);
 		
 		if (foundArticle == null) 
 			return id + "번 게시글은 존재하지 않습니다.";
 
-		articleDelete(foundArticle);
+		articles.remove(foundArticle);
 		return id + "번 게시글을 삭제하였습니다.";
-		
-		if (article != null) {
-			article.setTitle(null);
-			article.setBody(null);
-			return article.getId() + "번 게시글을 삭제하였습니다.";
-		}
-		
-		return "해당 게시글은 존재하지 않습니다.";
 	}
 	
-	private Article getArticleById(int id) {
+	public Article getArticleById(int id) {
 		Article foundArticle = null;
 
 		for (Article article : articles) {
--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -10,67 +10,31 @@ import com.example.demo.vo.Article;
 
 @Service
 public class ArticleService {
-	
-	private int lastArticleId;
-	private List<Article> articles;
 
 	private ArticleDao articleDao;
 
 	public ArticleService(ArticleDao articleDao) {
-		lastArticleId = 1;
-		articles = new ArrayList<>();
 		this.articleDao = articleDao;
-		
-		makeTestData();
-	}
-	
-	public void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = i + "번 게시글";
-			String body = i + "번 게시글 내용";
-			articles.add(new Article(lastArticleId++, title, body));
-		}
-	}
-	
-	///////////////////////////////////
-	
-	public Article articleWrite(String title, String body) {
-		return articleDao.articleWrite(lastArticleId, articles, title, body);
 	}
 
-	///////////////////////////////////
-	
+	public Article articleWrite(String title, String body) {
+		return articleDao.articleWrite(title, body);
+	}
+
 	public List<Article> articleList() {
-		return articles;
+		return articleDao.articleList();
 	}
 	
-	public String articleDetail(int id) {
+	public Object articleDetail(int id) {
 		return articleDao.articleDetail(id);
 	}
-	
-	///////////////////////////////////
 	
 	public String articleModify(int id, String title, String body) {
 		return articleDao.articleModify(id, title, body);
 	}
 	
-	///////////////////////////////////
-	
 	public String articleDelete(int id) {
 		return articleDao.articleDelete(id);
 	}
 	
-	///////////////////////////////////
-	
-	private Article getArticleById(int id) {
-		Article foundArticle = null;
-
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				foundArticle = article;
-			}
-		}
-		
-		return foundArticle;
-	}
 }
