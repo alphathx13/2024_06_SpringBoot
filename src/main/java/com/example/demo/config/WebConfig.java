6757778ca51e5c27310dddca3a87a915842ce016
alphathx13 <alphathx13@gmail.com>
2024-06-25 16:49:45 +0900

    6일차 interceptor의 활용

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -5,18 +5,33 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.example.demo.interceptor.BeforeActionInterceptor;
+import com.example.demo.interceptor.NeedLoginInterceptor;
+import com.example.demo.interceptor.NeedLogoutInterceptor;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer {
 	private BeforeActionInterceptor beforeActionInterceptor;
+	private NeedLoginInterceptor needLoginInterceptor;
+	private NeedLogoutInterceptor needLogoutInterceptor;
 
-	public WebConfig(BeforeActionInterceptor beforeActionInterceptor) {
+	public WebConfig(BeforeActionInterceptor beforeActionInterceptor, NeedLoginInterceptor needLoginInterceptor, NeedLogoutInterceptor needLogoutInterceptor) {
 		this.beforeActionInterceptor = beforeActionInterceptor;
+		this.needLoginInterceptor = needLoginInterceptor;
+		this.needLogoutInterceptor = needLogoutInterceptor;
 	}
-	
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**");
+		
+		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/article/write")
+				.addPathPatterns("/usr/article/doWrite").addPathPatterns("/usr/article/modify")
+				.addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/doDelete")
+				.addPathPatterns("/usr/member/doLogout");
+		
+		registry.addInterceptor(needLogoutInterceptor).addPathPatterns("/usr/article/join")
+		.addPathPatterns("/usr/article/doJoin").addPathPatterns("/usr/article/login")
+		.addPathPatterns("/usr/article/doLogin");
 		// "/**" -> 모든 요청
 	}
 }
--- src/main/java/com/example/demo/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/example/demo/interceptor/NeedLoginInterceptor.java
@@ -4,22 +4,28 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.example.demo.vo.Rq;
+import com.example.demo.util.Util;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class BeforeActionInterceptor implements HandlerInterceptor{
+public class NeedLoginInterceptor implements HandlerInterceptor{
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		
-		Rq rq = new Rq(request);
-
-		request.setAttribute("rq", rq);
+		System.out.println(request.getRequestURL()); 
+		
+		Rq rq = (Rq) request.getAttribute("rq");
+		
+		if (rq.getLoginMemberNumber() == 0) {
+			response.setContentType("text/html; charset=UTF-8");
+			response.getWriter().append(Util.jsReplace("해당 기능은 로그인 후 이용해주세요.", "/"));
+			return false;
+		}
 		
 		return HandlerInterceptor.super.preHandle(request, response, handler);
-		// true 라면 정상작동, false라면 controller로 넘어가지 못함
 	}
 }
